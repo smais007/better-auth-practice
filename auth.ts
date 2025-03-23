5b980f28b93001a265bdfc557ae0341ee0ee8665
smais007 <smaisshawon@gmail.com>
2025-03-23 22:54:04 +0600

    setup better auth

--- auth.ts
+++ auth.ts
@@ -1,9 +1,28 @@
 import { betterAuth, BetterAuthOptions } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./lib/prisma";
+import { sendEmail } from "./actions/email";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "mongodb",
   }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: true,
+  },
+
+  emailVerification: {
+    sendOnSignUp: true,
+    autoSignInAfterVerification: true,
+    sendVerificationEmail: async ({ user, token }) => {
+      const verificationUrl = `${process.env.BETTER_AUTH_URL}/api/auth/verify-email?token=${token}&callbackUrl=${process.env.EMAIL_VERIFICATION_REDIRECT_URL}`;
+
+      await sendEmail({
+        to: user.email,
+        subject: "Verify your email address",
+        text: `Click the link to verify your email: ${verificationUrl}`,
+      });
+    },
+  },
 } satisfies BetterAuthOptions);
